a7abf0391f8589577911f24b95d3531421d93e80
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-12-16 23:49:04 +0700

    Update: `flake8` ignore error code `B905` .  Refactor: `DailyShift` > move some code from method `resolve_overlap_shifts`  to `resolve_overlapped_outer_shifts`

--- pysla/daily_shifts.py
+++ pysla/daily_shifts.py
@@ -1,11 +1,15 @@
-from pydantic import RootModel
-from typing import List, Literal, Dict
+from pydantic import AfterValidator, RootModel
+from typing import Annotated, List, Literal, Dict, Tuple
 from datetime import datetime
 from .datetime_utilities import Milliseconds
-from .shift import Shift
+from .shift import Shift, RESOLVED_TWO_SHIFTS
 
 RESOLVE_SHIFTS_METHOD = Literal[
     "overlapped", "outer", "throw-error", "delete-both"
+]
+
+SORTED_SHIFTS_START = Annotated[
+    List[Shift], AfterValidator(lambda x: sorted(x, key=lambda y: y.start))
 ]
 
 
@@ -30,7 +34,7 @@ class DailyShift(RootModel):
         return self.root[idx]
 
     @classmethod
-    def sorted_shifts_start(cls, shifts: List[Shift]) -> List[Shift]:
+    def sorted_shifts_start(cls, shifts: List[Shift]) -> SORTED_SHIFTS_START:
         sorted_shifts = sorted(shifts, key=lambda x: x.start)
         return sorted_shifts
 
@@ -40,9 +44,10 @@ class DailyShift(RootModel):
     ) -> "DailyShift":
         sorted_shifts = cls.sorted_shifts_start(shifts)
         resolved = []
-        for idx in range(len(sorted_shifts) - 1):
-            tmp_shift = sorted_shifts[idx]
-            tmp_next_shift = sorted_shifts[idx + 1]
+
+        for idx, (tmp_shift, tmp_next_shift) in enumerate(
+            zip(sorted_shifts, sorted_shifts[1:])
+        ):
             tmp_resolve = tmp_shift.resolve(tmp_next_shift)
             if tmp_shift is None:
                 continue
@@ -57,31 +62,49 @@ class DailyShift(RootModel):
                         {tmp_shift}\n + At index {idx+1}: {tmp_next_shift}"
                     )
                 case _, "overlapped" | "outer":
-                    if len(tmp_resolve[method]) == 2:
-                        resolved.append(tmp_resolve[method][0])
-                        sorted_shifts[idx + 1] = tmp_resolve[method][1]
-                    elif len(tmp_resolve[method]) == 1:
-                        resolved.append(tmp_resolve[method][0])
-                        sorted_shifts[idx + 1] = tmp_resolve[method][0]
+                    (
+                        sorted_shifts,
+                        resolved,
+                    ) = cls.resolve_overlapped_outer_shifts(
+                        tmp_resolve, method, sorted_shifts, resolved
+                    )
         recheck_overlapped_shifts = cls.check_overlap_shifts(resolved)
         assert (
             len(recheck_overlapped_shifts) == 0
         ), f"Tried resolving, cant resolved these `Shift`s: {recheck_overlapped_shifts}"
+        return DailyShift(resolved)
 
-        @classmethod
-        def check_overlap_shifts(
-            cls, shifts: List[Shift], sorted_starts: bool = True
-        ) -> Dict[int, List[Shift]]:
-            overlapped = {}
-            sorted_shifts = (
-                shifts if sorted_starts else cls.sorted_shifts_start(shifts)
-            )
-            for idx, tmp_shift in enumerate(sorted_shifts[:-1]):
-                next_tmp_shift = sorted_shifts[idx + 1]
-                if not tmp_shift != next_tmp_shift:
-                    continue
-                overlapped[idx] = [tmp_shift, next_tmp_shift]
-            return overlapped
+    @staticmethod
+    def resolve_overlapped_outer_shifts(
+        tmp_resolve: RESOLVED_TWO_SHIFTS,
+        method: Literal["overlapped", "outer"],
+        sorted_original_shifts: SORTED_SHIFTS_START,
+        sorted_resolving_shifts: SORTED_SHIFTS_START,
+        tmp_shift_idx: int,
+    ) -> Tuple[SORTED_SHIFTS_START, SORTED_SHIFTS_START]:
+        match method:
+            case "outer":
+                sorted_resolving_shifts.append(tmp_resolve[method][0])
+                sorted_original_shifts[tmp_shift_idx + 1] = tmp_resolve[method][1]
+            case "overlapped":
+                sorted_resolving_shifts.append(tmp_resolve[method])
+                sorted_resolving_shifts[tmp_shift_idx + 1] = tmp_resolve[method]
+        return sorted_original_shifts, sorted_resolving_shifts
+
+    @classmethod
+    def check_overlap_shifts(
+        cls, shifts: List[Shift], sorted_starts: bool = True
+    ) -> Dict[int, List[Shift]]:
+        overlapped = {}
+        sorted_shifts = (
+            shifts if sorted_starts else cls.sorted_shifts_start(shifts)
+        )
+        for idx, tmp_shift in enumerate(sorted_shifts[:-1]):
+            next_tmp_shift = sorted_shifts[idx + 1]
+            if not tmp_shift != next_tmp_shift:
+                continue
+            overlapped[idx] = [tmp_shift, next_tmp_shift]
+        return overlapped
 
     def total_milliseconds(self) -> Milliseconds:
         return sum([shift.diff for shift in self])
