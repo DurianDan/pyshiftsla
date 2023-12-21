9e5d7e2b4a34b8caee243444ee38ecbd58203be4
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-12-22 02:03:21 +0700

    Update-dev: test `ShiftsBuilder` to calculate sla.

--- tests/test_shifts_builder.py
+++ tests/test_shifts_builder.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 from tests.test_objects.manual import TEST_YEAR
 from tests.test_objects.shifts_builder import (
@@ -10,3 +10,14 @@ def test_shifts_builder():
     US_WOMAN_lIVING_IN_VIETNAM_MATERNITY_LEAVE_6MONTHS_2024.build_shifts_from_daterange(
         from_date=date(TEST_YEAR, 1, 1), to_date=date(TEST_YEAR, 12, 30)
     )
+    millis_sla_new_year_day = (
+        US_WOMAN_lIVING_IN_VIETNAM_MATERNITY_LEAVE_6MONTHS_2024.calculate_sla(
+            start_deal=datetime(TEST_YEAR, 1, 1, 14),
+            end_deal=datetime(TEST_YEAR, 1, 2, 9, 30),
+            use_generated_shifts=True,
+        )
+    )
+    hours_sla_new_year_day = millis_sla_new_year_day / (1000 * 60 * 60)
+    assert (
+        hours_sla_new_year_day == 1.5
+    ), f"hours_sla_new_year_day should be 1.5, not {hours_sla_new_year_day}"
